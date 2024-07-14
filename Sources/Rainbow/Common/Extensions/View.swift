ecf2799c17620a931b38e520d9f2fad225fe3f6c
Jay Raval <jraval93@gmail.com>
2024-07-14 12:07:20 -0400

    Update View.swift

--- Sources/Rainbow/Common/Extensions/View.swift
+++ Sources/Rainbow/Common/Extensions/View.swift
@@ -46,4 +46,15 @@ public extension View {
             return AnyView(self)
         }
     }
+    
+    public func rainbowBorder(gradient: LinearGradient, cornerRadius: CGFloat = 0, lineWidth: CGFloat = 1) -> some View {
+        self.overlay(
+            RoundedRectangle(cornerRadius: cornerRadius)
+                .stroke(gradient, lineWidth: lineWidth)
+        )
+    }
+    
+    public func roundRect(cornerRadius: CGFloat) -> some View {
+        self.clipShape(RoundedRectangle(cornerRadius: cornerRadius))
+    }
 }
